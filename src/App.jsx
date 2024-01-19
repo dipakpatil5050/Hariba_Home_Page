749d62d04e4db470a75d301bc60db4fda2ec096d
Dipak Patil <dipakup.a04@gmail.com>
2024-01-19 18:06:34 +0530

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ function App() {
       <Navbar />
       <Home images={images} />
       <ProductCard />
+      <ProductCard />
+      <ProductCard />
     </>
   );
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,23 +1,17 @@
 import React from "react";
-// import images from "../images";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 function ProductCard() {
   return (
     <>
-      <div className="w-12 h-52 ml-40 rounded-xl p-44 border flex flex-row item-center m-10 bg-transparent">
-        <img
-          className="z-30 w-5"
-          src="https://haribadairyfarm.com/cdn/shop/files/fpoint711_1880x.jpg?v=1690267317"
-          alt=""
-        />
+      <div className="w-32 card h-52 ml-40 bg-gray-500 rounded-xl p-44 border flex flex-row item-center m-10">
+        {/* {img1.map((item) => (
+          <div key={index}>
+            <img src={item.img} alt="" />
+          </div>
+        ))}
+
+        <img className="" src={props.img} alt={props.alt} /> */}
       </div>
-      <img
-        src="https://haribadairyfarm.com/cdn/shop/files/fpoint711_1880x.jpg?v=1690267317"
-        alt=""
-      />
     </>
   );
 }
