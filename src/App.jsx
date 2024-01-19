ac9957e8586d107be2978fc1e35520549a236bd9
Dipak Patil <dipakup.a04@gmail.com>
2024-01-19 17:39:56 +0530

    Some Functionality Changes in Product Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import "./App.css";
 import Home from "./components/Home";
 import images from "./components/images.js";
-
 import Navbar from "./components/Navbar";
-import ProductCard from "./components/pages/ProductCard.jsx";
+import ProductCard from "./components/ProductCard.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home images={images} />
-      <ProductCard />
-      <ProductCard />
       <ProductCard />
     </>
   );
